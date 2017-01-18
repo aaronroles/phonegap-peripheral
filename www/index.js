115d895a1612254ff72677c713a2d1cec3167c69
Aaron Roles <aaron_roles93@hotmail.com>
2017-01-18 19:25:20 +0000

    added more error functions

--- www/index.js
+++ www/index.js
@@ -5,17 +5,28 @@ function onDeviceReady(){
 }
 
 var initResult = function(result){
-    bluetoothle.initializePeripheral(initPeripheralSuccess, error, {"request": true, "restoreKey":"bluetoothleplugin-peripheral"});
+    bluetoothle.initializePeripheral(initPeripheralSuccess, initError, {"request": true, "restoreKey":"bluetoothleplugin-peripheral"});
 }
 
 var initPeripheralSuccess = function(result){
-    bluetoothle.addService(serviceSuccess, error, {service: "12345"});
+    bluetoothle.addService(serviceSuccess, addServiceError, {service: "12345"});
 }
 
 var serviceSuccess = function(result){
-    bluetoothle.startAdvertising(adSuccess, error, {"service": "12345", "name": "The Peripheral!"});
+    bluetoothle.startAdvertising(adSuccess, startAdError, {"service": "12345", "name": "The Peripheral!"});
 }
 
-var error = function(){
-    alert("error");
+var initError = function(error){
+    alert("initError");
+    alert(error);
+}
+
+var addServiceError = function(error){
+    alert("addServiceError");
+    alert(error);
+}
+
+var startAdError = function(error){
+    alert("startAdError");
+    alert(error);
 }
